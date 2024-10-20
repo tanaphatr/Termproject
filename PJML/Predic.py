09677eef4d07408fffb3eb95cffbaaa16adf55f7
tanaphatr <tanaphatrwangtong@gmail.com>
2024-10-20 17:51:56 +0700

    ML API predict

--- PJML/Predic.py
+++ PJML/Predic.py
@@ -11,10 +11,11 @@ processed_data = preprocess_data(data)
 print(processed_data)
 
 # โหลดโมเดลที่ถูกบันทึกไว้
-bayesian_ridge_model = joblib.load('E:/Term project/models/bayesian_ridge_model.joblib')
-linear_regression_model = joblib.load('E:/Term project/models/linear_regression_model.joblib')
-orthogonal_matching_pursuit_model = joblib.load('E:/Term project/models/orthogonal_matching_pursuit_model.joblib')
-adaboost_regressor_model = joblib.load('E:/Term project/models/adaboost_regressor_model.joblib')
+# bayesian_ridge_model = joblib.load('E:/Term project/PJ/PJML/models/bayesian_ridge_model.joblib')
+linear_regression_model = joblib.load('E:/Term project/PJ/PJML/models/linear_regression_model.joblib')
+# orthogonal_matching_pursuit_model = joblib.load('E:/Term project/PJ/PJML/models/orthogonal_matching_pursuit_model.joblib')
+# adaboost_regressor_model = joblib.load('E:/Term project/PJ/PJML/models/adaboost_regressor_model.joblib')
+
 # Convert sale_date to datetime and extract relevant features
 data['sale_date'] = pd.to_datetime(data['sale_date'])
 data['year'] = data['sale_date'].dt.year
